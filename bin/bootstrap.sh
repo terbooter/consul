fe7e9b7d2c2dc3b364e0e444c407a9c328e0a606
terbooter <terbooter@gmail.com>
2016-08-04 11:23:06 +0300

    use internal UI

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,4 +2,4 @@
 echo Gettip my IP ...;
 curl 'https://api.ipify.org' > /ip;
 echo My IP is $(cat /ip);
-consul agent -config-file /conf.json -advertise $(cat /ip) -bootstrap -server -ui-dir=/consul/ui;
+consul agent -config-file /conf.json -advertise $(cat /ip) -bootstrap -server -ui;
